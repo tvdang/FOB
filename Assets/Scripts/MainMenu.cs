72ef4ff17dbfcde4213f29517ad1d237267bcefb
Tuan Dang <tvdang7@gmail.com>
2015-09-28 00:08:14 -0500

    first commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+
+	// Use this for initialization
+	public Canvas quitMenu;
+    public Canvas scoreMenu;
+    public Button startText;
+    public Button scoreText;
+    public Button aboutText;
+    public Button exitText;
+    
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        scoreMenu = scoreMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        scoreText = scoreText.GetComponent<Button>();
+        aboutText = aboutText.GetComponent<Button>();
+        
+        quitMenu.enabled = false;
+        scoreMenu.enabled = false;
+    }
+
+
+
+    public void aboutPress()   //about button press
+    {
+        Debug.Log("About Pressed");
+        Application.LoadLevel(2);
+
+    }
+
+
+    public void ScorePress()     // score button pressed open menu
+    {
+
+        quitMenu.enabled = false;
+        scoreMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+        scoreText.enabled = false;
+        aboutText.enabled = false;
+
+    }
+
+    public void NoScorePress()       //close score menu
+    {
+        quitMenu.enabled = false;
+        scoreMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+        scoreText.enabled = true;
+        aboutText.enabled = true;
+    }
+        public void ExitPress()         // exit press open up exit menu
+    {
+        Debug.Log("Exit Prssed");
+        quitMenu.enabled = true;
+        scoreMenu.enabled = false;
+        startText.enabled = false;
+        exitText.enabled = false;
+        scoreText.enabled = false;
+        aboutText.enabled = false;
+
+    }
+
+    
+
+	public void NoPress()           // exit button no press close exit menu
+    {
+        quitMenu.enabled = false;
+        scoreMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+        scoreText.enabled = true;
+        aboutText.enabled = true;
+    }
+	
+	public void StartLevel()        // start game button pressed opens main game
+    {
+        Application.LoadLevel(1);
+    }
+	
+    public void ExitGame()     //  exit menu yes press closes game
+    {
+        Application.Quit();
+
+    }
+	
+}
---
+++ Assets/Scripts/about.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class about : MonoBehaviour
+{
+
+    // Use this for initialization
+   
+    public Button backText;
+    void Start()
+    {
+        
+        backText = backText.GetComponent<Button>();
+      
+
+    }
+
+    // custom functions
+    public void backPress()
+    {
+        Application.LoadLevel(0);
+    }
+
+
+
+}
