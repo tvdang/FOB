8901191d6d45f2381fa2a9f2a035b4b32a2f8125
Tuan Dang <tvdang7@gmail.com>
2015-11-11 00:21:19 -0600

    not working

---
+++ Assets/Scripts/Randomspawn.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Randomspawn : MonoBehaviour
+{
+    public float timer = 0.0f;
+
+    public Transform swap1;
+    public Transform swap2;
+    public Transform swap3;
+
+    public Transform location;
+
+    public GameObject go;
+
+    public GameObject myPrefab;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        //Debug.Log(timer);
+        if (timer >= 2)
+
+            Swap();
+
+    }
+
+    void Swap()
+    {
+        timer = 0;
+
+        int randomPick = Mathf.Abs(Random.Range(1, 4));
+
+        if (randomPick == 1)
+        {
+            location = swap1;
+            Debug.Log("Pos 1");
+        }
+
+        if (randomPick == 2)
+        {
+            location = swap2;
+            Debug.Log("Pos 2");
+        }
+
+        if (randomPick == 3)
+        {
+            location = swap3;
+            Debug.Log("Pos 3");
+        }
+
+        go = Instantiate(myPrefab, location.position, location.rotation) as GameObject;
+
+        GetComponent<Rigidbody>().AddForce(location.forward * 100f);
+    }
+}
---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scoring : MonoBehaviour
+{
+    private float elapsedTime = 0; // The amount of time that has elapsed.
+    private int points = 100; // The player's current points.
+    private float score = 0;               // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update your timer every frame.
+        elapsedTime += Time.deltaTime;
+
+    }
+    float AssignPoints()
+    {
+      
+        score = elapsedTime * points;
+        return score;
+    }
+}
